239359be80c4456c02eef72178f919cf772f42ae
William Hughes <bill@webcdproductions.com>
2020-09-14 18:17:53 +0000

    This is the first commit

--- HelloWorldFormsApp1/Form1.cs
+++ HelloWorldFormsApp1/Form1.cs
@@ -20,7 +20,7 @@ namespace HelloWorldFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             //This is a test
-            lblTest.Text = "This is a test!!";
+            lblTest.Text = "This is a test!! This is ";
         }
     }
 }
